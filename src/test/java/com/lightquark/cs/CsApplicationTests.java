66eedc3ef584cc7d3e54aa9e5ae56c9ca733ba0b
Pavlo Shcherbak <lightquark@gmail.com>
2020-08-29 10:44:57 +0300

    Initial commit

---
+++ src/test/java/com/lightquark/cs/CsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightquark.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
